fa31f3ca3b155dad99533f47dbf25c68e647c0bb
Yoann Le Montagner <yo35@melix.net>
2017-09-24 18:27:06 +0200

    Re-organize test files

--- test/access.js
+++ test/access.js
@@ -25,6 +25,42 @@
 var RPBChess = require('../src/core.js');
 var test = require('unit.js');
 
+
+describe('Getters', function() {
+
+	var customFEN = 'rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b Kk e3 0 1';
+
+	it('Get board 1', function() { var p=new RPBChess.Position(); test.value(p.square('e1')).is('wk'); });
+	it('Get board 2', function() { var p=new RPBChess.Position(); test.value(p.square('f7')).is('bp'); });
+	it('Get board 3', function() { var p=new RPBChess.Position(); test.value(p.square('b4')).is('-'); });
+
+	it('Get turn 1', function() { var p=new RPBChess.Position(); test.value(p.turn()).is('w'); });
+	it('Get turn 2', function() { var p=new RPBChess.Position(customFEN); test.value(p.turn()).is('b'); });
+
+	it('Get castling 1', function() { var p=new RPBChess.Position(); test.value(p.castling('wq')).is(true); });
+	it('Get castling 2', function() { var p=new RPBChess.Position(customFEN); test.value(p.castling('bq')).is(false); });
+	it('Get castling 3', function() { var p=new RPBChess.Position(customFEN); test.value(p.castling('bk')).is(true); });
+
+	it('Get en-passant 1', function() { var p=new RPBChess.Position(); test.value(p.enPassant()).is('-'); });
+	it('Get en-passant 2', function() { var p=new RPBChess.Position(customFEN); test.value(p.enPassant()).is('e'); });
+
+	['j1', 'f9'].forEach(function(elem) {
+		it('Error for board with ' + elem, function() {
+			var p=new RPBChess.Position();
+			test.exception(function() { p.square(elem); }).isInstanceOf(RPBChess.exception.IllegalArgument);
+		});
+	});
+
+	['bK', 'wa'].forEach(function(elem) {
+		it('Error for castling with ' + elem, function() {
+			var p=new RPBChess.Position();
+			test.exception(function() { p.castling(elem); }).isInstanceOf(RPBChess.exception.IllegalArgument);
+		});
+	});
+
+});
+
+
 describe('Setters', function() {
 
 	var pos1 = new RPBChess.Position('start');
@@ -51,3 +87,4 @@ describe('Setters', function() {
 	it('Set en-passant 2b', function() { pos2.enPassant('h'); test.value(pos2.fen()).is('8/8/8/6K1/8/8/8/8 b q h3 0 1'); });
 
 });
+
