16ecf054c3886a56b7651dbd8b15a28858f29ad8
Yoann Le Montagner <yo35@melix.net>
2018-09-16 15:15:35 +0200

    Extract TokenStream

--- src/pgn.js
+++ src/pgn.js
@@ -28,231 +28,7 @@ var i18n = require('./i18n');
 
 var Position = require('./position').Position;
 var Game = require('./game').Game;
-
-
-// Conversion table NAG -> numeric code
-var SPECIAL_NAGS_LOOKUP = {
-	'!!' :  3,             // very good move
-	'!'  :  1,             // good move
-	'!?' :  5,             // interesting move
-	'?!' :  6,             // questionable move
-	'?'  :  2,             // bad move
-	'??' :  4,             // very bad move
-	'+-' : 18,             // White has a decisive advantage
-	'+/-': 16,             // White has a moderate advantage
-	'+/=': 14, '+=' : 14,  // White has a slight advantage
-	'='  : 10,             // equal position
-	'~'  : 13, 'inf': 13,  // unclear position
-	'=/+': 15, '=+' : 15,  // Black has a slight advantage
-	'-/+': 17,             // Black has a moderate advantage
-	'-+' : 19              // Black has a decisive advantage
-};
-
-
-/**
- * Parse a header value, unescaping special characters.
- *
- * @param {string} rawHeaderValue
- * @returns {string}
- * @ignore
- */
-function parseHeaderValue(rawHeaderValue) {
-	return rawHeaderValue.replace(/\\([\\"[\]])/g, '$1');
-}
-
-
-/**
- * Parse a comment, unescaping special characters, and looking for the `[%key value]` tags.
- *
- * @param {string} rawComment String to parse.
- * @returns {{comment:string, tags:Object}}
- * @ignore
- */
-function parseCommentValue(rawComment) {
-	rawComment = rawComment.replace(/\\([{}\\])/g, '$1');
-
-	var tags = {};
-
-	// Find and remove the tags from the raw comment.
-	var comment = rawComment.replace(/\[%([a-zA-Z]+) ([^[\]]+)\]/g, function(match, p1, p2) {
-		tags[p1] = p2;
-		return ' ';
-	});
-
-	// Trim the comment and collapse sequences of space characters into 1 character only.
-	comment = comment.replace(/^\s+|\s+$/g, '').replace(/\s+/g, ' ');
-	if(comment === '') {
-		comment = undefined;
-	}
-
-	// Return the result
-	return { comment:comment, tags:tags };
-}
-
-
-// PGN token types
-var /* const */ TOKEN_HEADER          = 1; // Ex: [White "Kasparov, G."]
-var /* const */ TOKEN_MOVE            = 2; // SAN notation or -- (with an optional move number)
-var /* const */ TOKEN_NAG             = 3; // $[1-9][0-9]* or a key from table SPECIAL_NAGS_LOOKUP (!!, +-, etc..)
-var /* const */ TOKEN_COMMENT         = 4; // {some text}
-var /* const */ TOKEN_BEGIN_VARIATION = 5; // (
-var /* const */ TOKEN_END_VARIATION   = 6; // )
-var /* const */ TOKEN_END_OF_GAME     = 7; // 1-0, 0-1, 1/2-1/2 or *
-
-
-/**
- * @class
- * @classdesc Stream of tokens.
- * @ignore
- */
-function TokenStream(pgnString) {
-	this.text           = pgnString; // what is being parsed
-	this._pos           = 0;         // current position in the string
-	this.emptyLineFound = false;     // whether an empty line has been encountered by skipBlank()
-	this.token          = 0;         // current token
-	this.tokenValue     = null;      // current token value (if any)
-	this.tokenPos       = 0;         // position of the current token in the string
-
-	// Space-like matchers
-	this._matchSpaces = /[ \f\t\v]+/g;
-	this._matchLineBreak = /\r?\n|\r/g;
-
-	// Token matchers
-	this._matchHeader = /\[\s*(\w+)\s+"((?:[^\\"[\]]|\\[\\"[\]])*)"\s*\]/g;
-	this._matchMove = /(?:[1-9][0-9]*\s*\.(?:\.\.)?\s*)?((?:O-O-O|O-O|[KQRBN][a-h]?[1-8]?x?[a-h][1-8]|(?:[a-h]x?)?[a-h][1-8](?:=?[KQRBNP])?)[+#]?|--)/g;
-	this._matchNag = /([!?][!?]?|\+\/?[-=]|[-=]\/?\+|=|inf|~)|\$([1-9][0-9]*)/g;
-	this._matchComment = /\{((?:[^{}\\]|\\[{}\\])*)\}/g;
-	this._matchBeginVariation = /\(/g;
-	this._matchEndVariation = /\)/g;
-	this._matchEndOfGame = /1-0|0-1|1\/2-1\/2|\*/g;
-
-	this._matchSpaces.matchedIndex = -1;
-	this._matchLineBreak.matchedIndex = -1;
-	this._matchHeader.matchedIndex = -1;
-	this._matchMove.matchedIndex = -1;
-	this._matchNag.matchedIndex = -1;
-	this._matchComment.matchedIndex = -1;
-	this._matchBeginVariation.matchedIndex = -1;
-	this._matchEndVariation.matchedIndex = -1;
-	this._matchEndOfGame.matchedIndex = -1;
-}
-
-
-/**
- * Try to match the given regular expression at the current position.
- *
- * @param {TokenStream} stream
- * @param {RegExp} re
- * @returns {boolean}
- * @ignore
- */
-function testAtPos(stream, re) {
-	if(re.matchedIndex < stream._pos) {
-		re.lastIndex = stream._pos;
-		re.matched = re.exec(stream.text);
-		re.matchedIndex = re.matched === null ? stream.text.length : re.matched.index;
-	}
-	if(re.matchedIndex === stream._pos) {
-		stream._pos = re.lastIndex;
-		return true;
-	}
-	else {
-		return false;
-	}
-}
-
-
-/**
- * Advance until the first non-blank character.
- *
- * @param {TokenStream} stream
- * @ignore
- */
-function skipBlanks(stream) {
-	var newLineCount = 0;
-	while(stream._pos < stream.text.length) {
-		if(testAtPos(stream, stream._matchSpaces)) {
-			// Nothing to do...
-		}
-		else if(testAtPos(stream, stream._matchLineBreak)) {
-			++newLineCount;
-		}
-		else {
-			break;
-		}
-	}
-
-	// An empty line was encountered if and only if at least to line breaks were found.
-	stream.emptyLineFound = newLineCount >= 2;
-}
-
-
-/**
- * Try to consume 1 token.
- *
- * @return {boolean} `true` if a token could have been read, `false` if the end of the text has been reached.
- * @throws {module:exception.InvalidPGN} If the text cannot be interpreted as a valid token.
- */
-TokenStream.prototype.consumeToken = function() {
-
-	// Consume blank (i.e. meaning-less) characters
-	skipBlanks(this);
-	if(this._pos >= this.text.length) {
-		return false; // -> `false` means that the end of the string have been reached
-	}
-
-	// Remaining part of the string
-	this.tokenPos = this._pos;
-
-	// Match a game header (ex: [White "Kasparov, G."])
-	if(testAtPos(this, this._matchHeader)) {
-		this.token      = TOKEN_HEADER;
-		this.tokenValue = { key: this._matchHeader.matched[1], value: parseHeaderValue(this._matchHeader.matched[2]) };
-	}
-
-	// Match a move or a null-move
-	else if(testAtPos(this, this._matchMove)) {
-		this.token      = TOKEN_MOVE;
-		this.tokenValue = this._matchMove.matched[1];
-	}
-
-	// Match a NAG
-	else if(testAtPos(this, this._matchNag)) {
-		this.token      = TOKEN_NAG;
-		this.tokenValue = this._matchNag.matched[2] === undefined ? SPECIAL_NAGS_LOOKUP[this._matchNag.matched[1]] : parseInt(this._matchNag.matched[2], 10);
-	}
-
-	// Match a comment
-	else if(testAtPos(this, this._matchComment)) {
-		this.token      = TOKEN_COMMENT;
-		this.tokenValue = parseCommentValue(this._matchComment.matched[1]);
-	}
-
-	// Match the beginning of a variation
-	else if(testAtPos(this, this._matchBeginVariation)) {
-		this.token      = TOKEN_BEGIN_VARIATION;
-		this.tokenValue = null;
-	}
-
-	// Match the end of a variation
-	else if(testAtPos(this, this._matchEndVariation)) {
-		this.token      = TOKEN_END_VARIATION;
-		this.tokenValue = null;
-	}
-
-	// Match a end-of-game marker
-	else if(testAtPos(this, this._matchEndOfGame)) {
-		this.token      = TOKEN_END_OF_GAME;
-		this.tokenValue = this._matchEndOfGame.matched[0];
-	}
-
-	// Otherwise, the string is badly formatted with respect to the PGN syntax
-	else {
-		throw new exception.InvalidPGN(this.text, this._pos, i18n.INVALID_PGN_TOKEN);
-	}
-
-	return true;
-};
+var TokenStream = require('./private_pgn/tokenstream').TokenStream;
 
 
 function parseNullableHeader(value) {
@@ -305,7 +81,7 @@ function processHeader(stream, game, initialPositionFactory, key, value) {
 		// initial position, that may be different from the usual one.
 		case 'FEN':
 			initialPositionFactory.fen = value;
-			initialPositionFactory.fenTokenPos = stream.tokenPos;
+			initialPositionFactory.fenTokenIndex = stream.tokenIndex();
 			break;
 
 		// The header 'Variant' indicates that this is not a regular chess game.
@@ -314,7 +90,7 @@ function processHeader(stream, game, initialPositionFactory, key, value) {
 				initialPositionFactory.variant = 'chess960';
 			}
 			else {
-				throw new exception.InvalidPGN(stream.text, stream.tokenPos, i18n.UNKNOWN_VARIANT, value);
+				throw stream.invalidPGNException(i18n.UNKNOWN_VARIANT, value);
 			}
 			break;
 	}
@@ -333,7 +109,7 @@ function initializeInitialPosition(stream, game, initialPositionFactory) {
 	}
 	catch(error) {
 		if(error instanceof exception.InvalidFEN) {
-			throw new exception.InvalidPGN(stream.text, initialPositionFactory.fenTokenPos, i18n.INVALID_FEN_IN_PGN_TEXT, error.message);
+			throw stream.invalidPGNException(initialPositionFactory.fenTokenIndex, i18n.INVALID_FEN_IN_PGN_TEXT, error.message);
 		}
 		else {
 			throw error;
@@ -369,32 +145,32 @@ function doParseGame(stream) {
 
 		// Matching anything else different from a header means that the move section
 		// is going to be parse => set-up the root node.
-		if(stream.token !== TOKEN_HEADER && node === null) {
+		if(stream.token() !== TokenStream.HEADER && node === null) {
 			initializeInitialPosition(stream, game, initialPositionFactory);
 			node = game.mainVariation();
 			nodeIsVariation = true;
 		}
 
 		// Token type switch
-		switch(stream.token) {
+		switch(stream.token()) {
 
 			// Header
-			case TOKEN_HEADER:
+			case TokenStream.HEADER:
 				if(node !== null) {
-					throw new exception.InvalidPGN(stream.text, stream.tokenPos, i18n.UNEXPECTED_PGN_HEADER);
+					throw stream.invalidPGNException(i18n.UNEXPECTED_PGN_HEADER);
 				}
-				processHeader(stream, game, initialPositionFactory, stream.tokenValue.key, stream.tokenValue.value);
+				processHeader(stream, game, initialPositionFactory, stream.tokenValue().key, stream.tokenValue().value);
 				break;
 
 			// Move or null-move
-			case TOKEN_MOVE:
+			case TokenStream.MOVE:
 				try {
-					node = node.play(stream.tokenValue);
+					node = node.play(stream.tokenValue());
 					nodeIsVariation = false;
 				}
 				catch(error) {
 					if(error instanceof exception.InvalidNotation) {
-						throw new exception.InvalidPGN(stream.text, stream.tokenPos, i18n.INVALID_MOVE_IN_PGN_TEXT, error.notation, error.message);
+						throw stream.invalidPGNException(i18n.INVALID_MOVE_IN_PGN_TEXT, error.notation, error.message);
 					}
 					else {
 						throw error;
@@ -403,53 +179,54 @@ function doParseGame(stream) {
 				break;
 
 			// NAG
-			case TOKEN_NAG:
-				node.addNag(stream.tokenValue);
+			case TokenStream.NAG:
+				node.addNag(stream.tokenValue());
 				break;
 
 			// Comment
-			case TOKEN_COMMENT:
-				for(var key in stream.tokenValue.tags) {
-					if(stream.tokenValue.tags[key] !== undefined) {
-						node.tag(key, stream.tokenValue.tags[key]);
+			case TokenStream.COMMENT:
+				var tags = stream.tokenValue().tags;
+				for(var key in tags) {
+					if(tags[key] !== undefined) {
+						node.tag(key, tags[key]);
 					}
 				}
-				node.comment(stream.tokenValue.comment, stream.emptyLineFound);
+				node.comment(stream.tokenValue().comment, stream.emptyLineFound());
 				break;
 
 			// Begin of variation
-			case TOKEN_BEGIN_VARIATION:
+			case TokenStream.BEGIN_VARIATION:
 				if(nodeIsVariation) {
-					throw new exception.InvalidPGN(stream.text, stream.tokenPos, i18n.UNEXPECTED_BEGIN_OF_VARIATION);
+					throw stream.invalidPGNException(i18n.UNEXPECTED_BEGIN_OF_VARIATION);
 				}
 				nodeStack.push(node);
-				node = node.addVariation(stream.emptyLineFound);
+				node = node.addVariation(stream.emptyLineFound());
 				nodeIsVariation = true;
 				break;
 
 			// End of variation
-			case TOKEN_END_VARIATION:
+			case TokenStream.END_VARIATION:
 				if(nodeStack.length === 0) {
-					throw new exception.InvalidPGN(stream.text, stream.tokenPos, i18n.UNEXPECTED_END_OF_VARIATION);
+					throw stream.invalidPGNException(i18n.UNEXPECTED_END_OF_VARIATION);
 				}
 				node = nodeStack.pop();
 				nodeIsVariation = false;
 				break;
 
 			// End-of-game
-			case TOKEN_END_OF_GAME:
+			case TokenStream.END_OF_GAME:
 				if(nodeStack.length > 0) {
-					throw new exception.InvalidPGN(stream.text, stream.tokenPos, i18n.UNEXPECTED_END_OF_GAME);
+					throw stream.invalidPGNException(i18n.UNEXPECTED_END_OF_GAME);
 				}
-				game.result(stream.tokenValue);
+				game.result(stream.tokenValue());
 				return game;
 
-		} // switch(token)
+		} // switch(stream.token())
 
-	} // while(consume(token()))
+	} // while(stream.consumeToken())
 
 	if(game !== null) {
-		throw new exception.InvalidPGN(stream.text, stream.text.length, i18n.UNEXPECTED_END_OF_TEXT);
+		throw stream.invalidPGNException(i18n.UNEXPECTED_END_OF_TEXT);
 	}
 	return null;
 }
@@ -465,8 +242,8 @@ function doParseGame(stream) {
  */
 function doSkipGame(stream) {
 	while(stream.consumeToken()) {
-		switch(stream.token) {
-			case TOKEN_END_OF_GAME: return true;
+		if(stream.token() === TokenStream.END_OF_GAME) {
+			return true;
 		}
 	}
 	return false;
@@ -490,7 +267,7 @@ function doSkipGame(stream) {
  * @throws {module:exception.InvalidPGN}
  */
 exports.pgnRead = function(pgnString, gameIndex) {
-	var stream = new TokenStream(pgnString);
+	var stream = new TokenStream(pgnString, 0);
 
 	// Parse all games...
 	if(arguments.length === 1) {
---
+++ src/private_pgn/tokenstream.js
@@ -0,0 +1,283 @@
+/******************************************************************************
+ *                                                                            *
+ *    This file is part of Kokopu, a JavaScript chess library.                *
+ *    Copyright (C) 2018  Yoann Le Montagner <yo35 -at- melix.net>            *
+ *                                                                            *
+ *    This program is free software: you can redistribute it and/or           *
+ *    modify it under the terms of the GNU Lesser General Public License      *
+ *    as published by the Free Software Foundation, either version 3 of       *
+ *    the License, or (at your option) any later version.                     *
+ *                                                                            *
+ *    This program is distributed in the hope that it will be useful,         *
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the            *
+ *    GNU Lesser General Public License for more details.                     *
+ *                                                                            *
+ *    You should have received a copy of the GNU Lesser General               *
+ *    Public License along with this program. If not, see                     *
+ *    <http://www.gnu.org/licenses/>.                                         *
+ *                                                                            *
+ ******************************************************************************/
+
+
+'use strict';
+
+
+var exception = require('../exception');
+var i18n = require('../i18n');
+
+
+/**
+ * @class
+ * @classdesc Stream of tokens.
+ */
+var TokenStream = exports.TokenStream = function(pgnString, initialPos) {
+	this._text           = pgnString;  // what is being parsed
+	this._pos            = initialPos; // current position in the string
+	this._emptyLineFound = false;      // whether an empty line has been encountered while parsing the current token
+	this._token          = 0;          // current token
+	this._tokenValue     = null;       // current token value (if any)
+	this._tokenIndex     = 0;          // position of the current token in the string
+
+	// Space-like matchers
+	this._matchSpaces = /[ \f\t\v]+/g;
+	this._matchLineBreak = /\r?\n|\r/g;
+
+	// Token matchers
+	this._matchHeader = /\[\s*(\w+)\s+"((?:[^\\"[\]]|\\[\\"[\]])*)"\s*\]/g;
+	this._matchMove = /(?:[1-9][0-9]*\s*\.(?:\.\.)?\s*)?((?:O-O-O|O-O|[KQRBN][a-h]?[1-8]?x?[a-h][1-8]|(?:[a-h]x?)?[a-h][1-8](?:=?[KQRBNP])?)[+#]?|--)/g;
+	this._matchNag = /([!?][!?]?|\+\/?[-=]|[-=]\/?\+|=|inf|~)|\$([1-9][0-9]*)/g;
+	this._matchComment = /\{((?:[^{}\\]|\\[{}\\])*)\}/g;
+	this._matchBeginVariation = /\(/g;
+	this._matchEndVariation = /\)/g;
+	this._matchEndOfGame = /1-0|0-1|1\/2-1\/2|\*/g;
+
+	this._matchSpaces.matchedIndex = -1;
+	this._matchLineBreak.matchedIndex = -1;
+	this._matchHeader.matchedIndex = -1;
+	this._matchMove.matchedIndex = -1;
+	this._matchNag.matchedIndex = -1;
+	this._matchComment.matchedIndex = -1;
+	this._matchBeginVariation.matchedIndex = -1;
+	this._matchEndVariation.matchedIndex = -1;
+	this._matchEndOfGame.matchedIndex = -1;
+};
+
+
+// PGN token types
+var TOKEN_HEADER          = TokenStream.HEADER          = 1; // Ex: [White "Kasparov, G."]
+var TOKEN_MOVE            = TokenStream.MOVE            = 2; // SAN notation or -- (with an optional move number)
+var TOKEN_NAG             = TokenStream.NAG             = 3; // $[1-9][0-9]* or a key from table SPECIAL_NAGS_LOOKUP (!!, +-, etc..)
+var TOKEN_COMMENT         = TokenStream.COMMENT         = 4; // {some text}
+var TOKEN_BEGIN_VARIATION = TokenStream.BEGIN_VARIATION = 5; // (
+var TOKEN_END_VARIATION   = TokenStream.END_VARIATION   = 6; // )
+var TOKEN_END_OF_GAME     = TokenStream.END_OF_GAME     = 7; // 1-0, 0-1, 1/2-1/2 or *
+
+
+/**
+ * Try to match the given regular expression at the current position.
+ *
+ * @param {TokenStream} stream
+ * @param {RegExp} regex
+ * @returns {boolean}
+ */
+function testAtPos(stream, regex) {
+	if(regex.matchedIndex < stream._pos) {
+		regex.lastIndex = stream._pos;
+		regex.matched = regex.exec(stream._text);
+		regex.matchedIndex = regex.matched === null ? stream._text.length : regex.matched.index;
+	}
+
+	if(regex.matchedIndex === stream._pos) {
+		stream._pos = regex.lastIndex;
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+
+/**
+ * Advance until the first non-blank character.
+ *
+ * @param {TokenStream} stream
+ */
+function skipBlanks(stream) {
+	var newLineCount = 0;
+	while(stream._pos < stream._text.length) {
+		if(testAtPos(stream, stream._matchSpaces)) {
+			// Nothing to do...
+		}
+		else if(testAtPos(stream, stream._matchLineBreak)) {
+			++newLineCount;
+		}
+		else {
+			break;
+		}
+	}
+
+	// An empty line was encountered if and only if at least to line breaks were found.
+	stream._emptyLineFound = newLineCount >= 2;
+}
+
+
+/**
+ * Parse a header value, unescaping special characters.
+ *
+ * @param {string} rawHeaderValue
+ * @returns {string}
+ */
+function parseHeaderValue(rawHeaderValue) {
+	return rawHeaderValue.replace(/\\([\\"[\]])/g, '$1');
+}
+
+
+/**
+ * Parse a comment, unescaping special characters, and looking for the `[%key value]` tags.
+ *
+ * @param {string} rawComment String to parse.
+ * @returns {{comment:string, tags:Object}}
+ */
+function parseCommentValue(rawComment) {
+	rawComment = rawComment.replace(/\\([{}\\])/g, '$1');
+
+	var tags = {};
+
+	// Find and remove the tags from the raw comment.
+	var comment = rawComment.replace(/\[%([a-zA-Z]+) ([^[\]]+)\]/g, function(match, p1, p2) {
+		tags[p1] = p2;
+		return ' ';
+	});
+
+	// Trim the comment and collapse sequences of space characters into 1 character only.
+	comment = comment.replace(/^\s+|\s+$/g, '').replace(/\s+/g, ' ');
+	if(comment === '') {
+		comment = undefined;
+	}
+
+	// Return the result
+	return { comment:comment, tags:tags };
+}
+
+
+// Conversion table NAG -> numeric code
+var SPECIAL_NAGS_LOOKUP = {
+	'!!' :  3,             // very good move
+	'!'  :  1,             // good move
+	'!?' :  5,             // interesting move
+	'?!' :  6,             // questionable move
+	'?'  :  2,             // bad move
+	'??' :  4,             // very bad move
+	'+-' : 18,             // White has a decisive advantage
+	'+/-': 16,             // White has a moderate advantage
+	'+/=': 14, '+=' : 14,  // White has a slight advantage
+	'='  : 10,             // equal position
+	'~'  : 13, 'inf': 13,  // unclear position
+	'=/+': 15, '=+' : 15,  // Black has a slight advantage
+	'-/+': 17,             // Black has a moderate advantage
+	'-+' : 19              // Black has a decisive advantage
+};
+
+
+/**
+ * Try to consume 1 token.
+ *
+ * @return {boolean} `true` if a token could have been read, `false` if the end of the text has been reached.
+ * @throws {module:exception.InvalidPGN} If the text cannot be interpreted as a valid token.
+ */
+TokenStream.prototype.consumeToken = function() {
+
+	// Consume blank (i.e. meaning-less) characters
+	skipBlanks(this);
+	if(this._pos >= this._text.length) {
+		this._tokenIndex = this._text.length;
+		return false;
+	}
+
+	// Remaining part of the string
+	this._tokenIndex = this._pos;
+
+	// Match a game header (ex: [White "Kasparov, G."])
+	if(testAtPos(this, this._matchHeader)) {
+		this._token      = TOKEN_HEADER;
+		this._tokenValue = { key: this._matchHeader.matched[1], value: parseHeaderValue(this._matchHeader.matched[2]) };
+	}
+
+	// Match a move or a null-move
+	else if(testAtPos(this, this._matchMove)) {
+		this._token      = TOKEN_MOVE;
+		this._tokenValue = this._matchMove.matched[1];
+	}
+
+	// Match a NAG
+	else if(testAtPos(this, this._matchNag)) {
+		this._token      = TOKEN_NAG;
+		this._tokenValue = this._matchNag.matched[2] === undefined ? SPECIAL_NAGS_LOOKUP[this._matchNag.matched[1]] :
+			parseInt(this._matchNag.matched[2], 10);
+	}
+
+	// Match a comment
+	else if(testAtPos(this, this._matchComment)) {
+		this._token      = TOKEN_COMMENT;
+		this._tokenValue = parseCommentValue(this._matchComment.matched[1]);
+	}
+
+	// Match the beginning of a variation
+	else if(testAtPos(this, this._matchBeginVariation)) {
+		this._token      = TOKEN_BEGIN_VARIATION;
+		this._tokenValue = null;
+	}
+
+	// Match the end of a variation
+	else if(testAtPos(this, this._matchEndVariation)) {
+		this._token      = TOKEN_END_VARIATION;
+		this._tokenValue = null;
+	}
+
+	// Match a end-of-game marker
+	else if(testAtPos(this, this._matchEndOfGame)) {
+		this._token      = TOKEN_END_OF_GAME;
+		this._tokenValue = this._matchEndOfGame.matched[0];
+	}
+
+	// Otherwise, the string is badly formatted with respect to the PGN syntax
+	else {
+		throw new exception.InvalidPGN(this._text, this._pos, i18n.INVALID_PGN_TOKEN);
+	}
+
+	return true;
+};
+
+
+TokenStream.prototype.emptyLineFound = function() {
+	return this._emptyLineFound;
+};
+
+
+TokenStream.prototype.token = function() {
+	return this._token;
+};
+
+
+TokenStream.prototype.tokenValue = function() {
+	return this._tokenValue;
+};
+
+
+TokenStream.prototype.tokenIndex = function() {
+	return this._tokenIndex;
+};
+
+
+TokenStream.prototype.invalidPGNException = function(tokenIndex) {
+	var constructorArguments = [ this._text ];
+	if(typeof tokenIndex !== 'number') {
+		constructorArguments.push(this._tokenIndex);
+	}
+	Array.prototype.push.apply(constructorArguments, arguments);
+
+	var result = Object.create(exception.InvalidPGN.prototype);
+	exception.InvalidPGN.apply(result, constructorArguments);
+	return result;
+};
