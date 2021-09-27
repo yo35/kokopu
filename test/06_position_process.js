b310c5667c8aa4221b716f21d3a12e61f87344dc
Yoann Le Montagner <yo35@melix.net>
2021-09-27 17:40:17 +0200

    Re-label unit tests on Position

--- test/06_position_process.js
+++ test/06_position_process.js
@@ -28,8 +28,8 @@ var readCSV = require('./common/readcsv');
 var test = require('unit.js');
 
 
-function testData() {
-	return readCSV('positions.csv', function(fields) {
+function itForEach(fun) {
+	readCSV('positions.csv', function(fields) {
 		return {
 			label       : fields[ 0],
 			constructor : fields[ 1],
@@ -48,6 +48,10 @@ function testData() {
 			notations   : fields[14],
 			successors  : fields[15]
 		};
+	}).forEach(function(elem) {
+		it(elem.label, function() {
+			fun(elem);
+		});
 	});
 }
 
@@ -64,138 +68,122 @@ function createPosition(testDataDescriptor) {
 
 
 describe('Variant getter', function() {
-	testData().forEach(function(elem) {
-		it('Position ' + elem.label, function() {
-			var pos = createPosition(elem);
-			test.value(pos.variant()).is(elem.variant);
-		});
+	itForEach(function(elem) {
+		var pos = createPosition(elem);
+		test.value(pos.variant()).is(elem.variant);
 	});
 });
 
 
 describe('Turn getter', function() {
-	testData().forEach(function(elem) {
-		it('Position ' + elem.label, function() {
-			var pos = createPosition(elem);
-			test.value(pos.turn()).is(elem.turn);
-		});
+	itForEach(function(elem) {
+		var pos = createPosition(elem);
+		test.value(pos.turn()).is(elem.turn);
 	});
 });
 
 
 describe('Legality check & king squares', function() {
-	testData().forEach(function(elem) {
-		it('Position ' + elem.label, function() {
-			var pos = createPosition(elem);
-			test.value(pos.isLegal()).is(elem.isLegal);
-			test.value(pos.kingSquare('w')).is(elem.whiteKing);
-			test.value(pos.kingSquare('b')).is(elem.blackKing);
-		});
+	itForEach(function(elem) {
+		var pos = createPosition(elem);
+		test.value(pos.isLegal()).is(elem.isLegal);
+		test.value(pos.kingSquare('w')).is(elem.whiteKing);
+		test.value(pos.kingSquare('b')).is(elem.blackKing);
 	});
 });
 
 
 describe('Check / checkmate / stalemate', function() {
-	testData().forEach(function(elem) {
-		it('Position ' + elem.label, function() {
-			var pos = createPosition(elem);
-			test.value(pos.isCheck()).is(elem.isCheck);
-			test.value(pos.isCheckmate()).is(elem.isCheckmate);
-			test.value(pos.isStalemate()).is(elem.isStalemate);
-			test.value(pos.hasMove()).is(elem.hasMove);
-		});
+	itForEach(function(elem) {
+		var pos = createPosition(elem);
+		test.value(pos.isCheck()).is(elem.isCheck);
+		test.value(pos.isCheckmate()).is(elem.isCheckmate);
+		test.value(pos.isStalemate()).is(elem.isStalemate);
+		test.value(pos.hasMove()).is(elem.hasMove);
 	});
 });
 
 
 describe('Move generation', function() {
-	testData().forEach(function(elem) {
-		it('Position ' + elem.label, function() {
-			var moves = createPosition(elem).moves().map(function(move) { return move.toString(); }).sort();
-			test.value(moves.join('/')).is(elem.moves);
-		});
+	itForEach(function(elem) {
+		var moves = createPosition(elem).moves().map(function(move) { return move.toString(); }).sort();
+		test.value(moves.join('/')).is(elem.moves);
 	});
 });
 
 
 describe('Move legality check', function() {
-	testData().forEach(function(elem) {
-		it('Position ' + elem.label, function() {
-			var moves = [];
-			var pos = createPosition(elem);
+	itForEach(function(elem) {
+		var moves = [];
+		var pos = createPosition(elem);
 
-			kokopu.forEachSquare(function(from) {
-				kokopu.forEachSquare(function(to) {
+		kokopu.forEachSquare(function(from) {
+			kokopu.forEachSquare(function(to) {
 
-					var moveDescriptor = pos.isMoveLegal(from, to);
-					if(!moveDescriptor) {
-						return;
-					}
+				var moveDescriptor = pos.isMoveLegal(from, to);
+				if(!moveDescriptor) {
+					return;
+				}
 
-					switch(moveDescriptor.status) {
+				switch(moveDescriptor.status) {
 
-						case 'regular':
-							moves.push(moveDescriptor());
-							break;
+					case 'regular':
+						moves.push(moveDescriptor());
+						break;
 
-						case 'promotion':
-							if (pos.variant() === 'antichess') {
-								moves.push(moveDescriptor('k'));
-							}
-							moves.push(moveDescriptor('q'));
-							moves.push(moveDescriptor('r'));
-							moves.push(moveDescriptor('b'));
-							moves.push(moveDescriptor('n'));
-							break;
+					case 'promotion':
+						if (pos.variant() === 'antichess') {
+							moves.push(moveDescriptor('k'));
+						}
+						moves.push(moveDescriptor('q'));
+						moves.push(moveDescriptor('r'));
+						moves.push(moveDescriptor('b'));
+						moves.push(moveDescriptor('n'));
+						break;
 
-						default:
-							break;
-					}
-				});
+					default:
+						break;
+				}
 			});
-
-			test.value(moves.map(function(move) { return move.toString(); }).sort().join('/')).is(elem.moves);
 		});
+
+		test.value(moves.map(function(move) { return move.toString(); }).sort().join('/')).is(elem.moves);
 	});
 });
 
 
 describe('Play', function() {
-	testData().forEach(function(elem) {
-		it('Position ' + elem.label, function() {
-			var initialPos = createPosition(elem);
-			var moves = initialPos.moves().sort(function(e1, e2) { return e1.toString().localeCompare(e2.toString()); });
-			var successors = moves.map(function(move) {
-				var nextPos = new kokopu.Position(initialPos);
-				nextPos.play(move);
-				return nextPos.fen();
-			});
-			test.value(successors.join('|')).is(elem.successors);
+	itForEach(function(elem) {
+		var initialPos = createPosition(elem);
+		var moves = initialPos.moves().sort(function(e1, e2) { return e1.toString().localeCompare(e2.toString()); });
+		var successors = moves.map(function(move) {
+			var nextPos = new kokopu.Position(initialPos);
+			nextPos.play(move);
+			return nextPos.fen();
 		});
+		test.value(successors.join('|')).is(elem.successors);
 	});
 });
 
 
 function notationGenerationTest(elem, expectedNotations, testedFunction) {
-	return function() {
-		var pos = createPosition(elem);
-		var moves = pos.moves().sort(function(e1, e2) { return e1.toString().localeCompare(e2.toString()); });
-		var actionNotations = moves.map(function(move) { return testedFunction(pos, move); });
-		test.value(actionNotations.join('/')).is(expectedNotations);
-	};
+	var pos = createPosition(elem);
+	var moves = pos.moves().sort(function(e1, e2) { return e1.toString().localeCompare(e2.toString()); });
+	var actionNotations = moves.map(function(move) { return testedFunction(pos, move); });
+	test.value(actionNotations.join('/')).is(expectedNotations);
 }
 
 
 describe('UCI notation generation', function() {
-	testData().forEach(function(elem) {
-		it('Position ' + elem.label, notationGenerationTest(elem, elem.uciMoves, function(pos, move) { return pos.uci(move); }));
+	itForEach(function(elem) {
+		notationGenerationTest(elem, elem.uciMoves, function(pos, move) { return pos.uci(move); });
 	});
 });
 
 
 describe('Standard algebraic notation generation', function() {
-	testData().forEach(function(elem) {
-		it('Position ' + elem.label, notationGenerationTest(elem, elem.notations, function(pos, move) { return pos.notation(move); }));
+	itForEach(function(elem) {
+		notationGenerationTest(elem, elem.notations, function(pos, move) { return pos.notation(move); });
 	});
 });
 
@@ -205,44 +193,42 @@ describe('Figurine notation generation', function() {
 	var /* const */ WHITE_FIGURINES = { 'K':'\u2654', 'Q':'\u2655', 'R':'\u2656', 'B':'\u2657', 'N':'\u2658', 'P':'\u2659'};
 	var /* const */ BLACK_FIGURINES = { 'K':'\u265a', 'Q':'\u265b', 'R':'\u265c', 'B':'\u265d', 'N':'\u265e', 'P':'\u265f'};
 
-	testData().forEach(function(elem) {
+	itForEach(function(elem) {
 		var figNotations = elem.notations.replace(/[KQRBNP]/g, function(val) { return elem.turn === 'w' ? WHITE_FIGURINES[val] : BLACK_FIGURINES[val]; });
-		it('Position ' + elem.label, notationGenerationTest(elem, figNotations, function(pos, move) { return pos.figurineNotation(move); }));
+		notationGenerationTest(elem, figNotations, function(pos, move) { return pos.figurineNotation(move); });
 	});
 });
 
 
 describe('UCI notation parsing', function() {
 	var /* const */ PROMO  = ['', 'k', 'q', 'r', 'b', 'n', 'p'];
-	testData().forEach(function(elem) {
-		it('Position ' + elem.label, function() {
-			var pos = createPosition(elem);
-			var moves = [];
+	itForEach(function(elem) {
+		var pos = createPosition(elem);
+		var moves = [];
 
-			// Try all the possible UCI notations...
-			kokopu.forEachSquare(function(from) {
-				kokopu.forEachSquare(function(to) {
-					for(var p=0; p<PROMO.length; ++p) {
-						var text = from + to + PROMO[p];
-						try {
-							var descriptor = pos.uci(text);
-							moves.push(descriptor.toString());
-						}
-						catch(e) {
-							if(!(e instanceof kokopu.exception.InvalidNotation)) {
-								throw e;
-							}
+		// Try all the possible UCI notations...
+		kokopu.forEachSquare(function(from) {
+			kokopu.forEachSquare(function(to) {
+				for(var p=0; p<PROMO.length; ++p) {
+					var text = from + to + PROMO[p];
+					try {
+						var descriptor = pos.uci(text);
+						moves.push(descriptor.toString());
+					}
+					catch(e) {
+						if(!(e instanceof kokopu.exception.InvalidNotation)) {
+							throw e;
 						}
 					}
-				});
+				}
 			});
-
-			// Sort the moves and remove the duplicates.
-			moves.sort();
-			moves = moves.filter(function(move, index, tab) { return index === 0 || move !== tab[index-1]; });
-
-			test.value(moves.join('/')).is(elem.moves);
 		});
+
+		// Sort the moves and remove the duplicates.
+		moves.sort();
+		moves = moves.filter(function(move, index, tab) { return index === 0 || move !== tab[index-1]; });
+
+		test.value(moves.join('/')).is(elem.moves);
 	});
 });
 
@@ -252,70 +238,68 @@ function notationParsingTest(elem, testedPieces, testedPromos, testedFunction) {
 	var /* const */ RANK_DISAMBIGUATION = ['', '1', '2', '3', '4', '5', '6', '7', '8'];
 	var /* const */ FILE_DISAMBIGUATION = ['', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
 
-	return function() {
-		var pos = createPosition(elem);
-		var moves = [];
+	var pos = createPosition(elem);
+	var moves = [];
 
-		// Catch the exceptions thrown by the parsing function.
-		function parseNotation(text) {
-			try {
-				var descriptor = testedFunction(pos, text);
-				moves.push(descriptor.toString());
-			}
-			catch(e) {
-				if(!(e instanceof kokopu.exception.InvalidNotation)) {
-					throw e;
-				}
+	// Catch the exceptions thrown by the parsing function.
+	function parseNotation(text) {
+		try {
+			var descriptor = testedFunction(pos, text);
+			moves.push(descriptor.toString());
+		}
+		catch(e) {
+			if(!(e instanceof kokopu.exception.InvalidNotation)) {
+				throw e;
 			}
 		}
+	}
 
-		// Castling moves
-		parseNotation('O-O-O');
-		parseNotation('O-O');
+	// Castling moves
+	parseNotation('O-O-O');
+	parseNotation('O-O');
 
-		// Pawn move
-		kokopu.forEachSquare(function(to) {
-			for(var fd=0; fd<FILE_DISAMBIGUATION.length; ++fd) {
-				for(var p=0; p<testedPromos.length; ++p) {
-					var text = FILE_DISAMBIGUATION[fd] + to + testedPromos[p];
+	// Pawn move
+	kokopu.forEachSquare(function(to) {
+		for(var fd=0; fd<FILE_DISAMBIGUATION.length; ++fd) {
+			for(var p=0; p<testedPromos.length; ++p) {
+				var text = FILE_DISAMBIGUATION[fd] + to + testedPromos[p];
+				parseNotation(text);
+			}
+		}
+	});
+
+	// Non-pawn moves
+	kokopu.forEachSquare(function(to) {
+		for(var p=0; p<testedPieces.length; ++p) {
+			for(var rd=0; rd<RANK_DISAMBIGUATION.length; ++rd) {
+				for(var fd=0; fd<FILE_DISAMBIGUATION.length; ++fd) {
+					var text = testedPieces[p] + FILE_DISAMBIGUATION[fd] + RANK_DISAMBIGUATION[rd] + to;
 					parseNotation(text);
 				}
 			}
-		});
+		}
+	});
 
-		// Non-pawn moves
-		kokopu.forEachSquare(function(to) {
-			for(var p=0; p<testedPieces.length; ++p) {
-				for(var rd=0; rd<RANK_DISAMBIGUATION.length; ++rd) {
-					for(var fd=0; fd<FILE_DISAMBIGUATION.length; ++fd) {
-						var text = testedPieces[p] + FILE_DISAMBIGUATION[fd] + RANK_DISAMBIGUATION[rd] + to;
-						parseNotation(text);
-					}
-				}
-			}
-		});
+	// Sort the moves and remove the duplicates.
+	moves.sort();
+	moves = moves.filter(function(move, index, tab) { return index === 0 || move !== tab[index-1]; });
 
-		// Sort the moves and remove the duplicates.
-		moves.sort();
-		moves = moves.filter(function(move, index, tab) { return index === 0 || move !== tab[index-1]; });
-
-		test.value(moves.join('/')).is(elem.moves);
-	};
+	test.value(moves.join('/')).is(elem.moves);
 }
 
 
 describe('Standard algebraic notation parsing', function() {
-	testData().forEach(function(elem) {
-		it('Position ' + elem.label, notationParsingTest(elem, 'KQRBN', ['', 'K', 'Q', 'R', 'B', 'N', 'P'], function(pos, text) { return pos.notation(text, false); }));
+	itForEach(function(elem) {
+		notationParsingTest(elem, 'KQRBN', ['', 'K', 'Q', 'R', 'B', 'N', 'P'], function(pos, text) { return pos.notation(text, false); });
 	});
 });
 
 
 describe('Figurine notation parsing', function() {
-	testData().forEach(function(elem) {
+	itForEach(function(elem) {
 		var testedPieces = elem.turn === 'w' ? '\u2654\u2655\u2656\u2657\u2658' : '\u265a\u265b\u265c\u265d\u265e';
 		var testedPromos = elem.turn === 'w' ? ['', '\u2654', '\u2655', '\u2656', '\u2657', '\u2658', '\u2659'] :
 			['', '\u265a', '\u265b', '\u265c', '\u265d', '\u265e', '\u265f'];
-		it('Position ' + elem.label, notationParsingTest(elem, testedPieces, testedPromos, function(pos, text) { return pos.figurineNotation(text, false); }));
+		notationParsingTest(elem, testedPieces, testedPromos, function(pos, text) { return pos.figurineNotation(text, false); });
 	});
 });
