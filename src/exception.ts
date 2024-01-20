a99c39c235aa6eeb217319bdac9bd52dc14be6ff
Yoann Le Montagner <yo35@melix.net>
2024-01-20 11:50:14 +0100

    Add homepage in copyright notice + package.json

--- src/exception.ts
+++ src/exception.ts
@@ -1,14 +1,16 @@
-/* -------------------------------------------------------------------------- *
+/*!
+ * -------------------------------------------------------------------------- *
  *                                                                            *
- *    This file is part of Kokopu, a JavaScript chess library.                *
+ *    Kokopu - A JavaScript/TypeScript chess library.                         *
+ *    <https://www.npmjs.com/package/kokopu>                                  *
  *    Copyright (C) 2018-2024  Yoann Le Montagner <yo35 -at- melix.net>       *
  *                                                                            *
- *    This program is free software: you can redistribute it and/or           *
+ *    Kokopu is free software: you can redistribute it and/or                 *
  *    modify it under the terms of the GNU Lesser General Public License      *
  *    as published by the Free Software Foundation, either version 3 of       *
  *    the License, or (at your option) any later version.                     *
  *                                                                            *
- *    This program is distributed in the hope that it will be useful,         *
+ *    Kokopu is distributed in the hope that it will be useful,               *
  *    but WITHOUT ANY WARRANTY; without even the implied warranty of          *
  *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the            *
  *    GNU Lesser General Public License for more details.                     *
